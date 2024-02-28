ebf66fcc6db67adab6f62599c9b2a3fb9eb3a938
Turky0987 <144216105+Turky0987@users.noreply.github.com>
2024-02-28 10:20:29 +0100

    Add project files.

---
+++ uppgift 6.7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tal = 17; 
+
+        if (ÄrPrimtal(tal))
+        {
+            Console.WriteLine(tal + " är ett primtal.");
+        }
+        else
+        {
+            Console.WriteLine(tal + " är inte ett primtal.");
+        }
+    }
+
+    static bool ÄrPrimtal(int tal)
+    {
+        if (tal <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(tal); i++)
+        {
+            if (tal % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
